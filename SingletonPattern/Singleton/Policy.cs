2a2b27112a2b3a2c1853665f1b104283d5e34829
Talha Çağatay IŞIK <talhacagatayisik@gmail.com>
2018-08-19 16:56:32 +0300

    SingletonPattern

---
+++ SingletonPattern/Singleton/Policy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class Policy
+    {
+        //private static readonly object _lock = new object();
+        private static readonly Policy _instance = new Policy();
+        public static Policy Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+        public Policy()
+        {
+
+        }
+        private int Id { get; set; } = 123;
+        private string Insured { get; set; } = "Talha Çağatay IŞIK";
+
+        public string GetInsuredName() => Insured;
+    }
+}
